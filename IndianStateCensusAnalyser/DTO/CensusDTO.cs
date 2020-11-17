f5044b3fa192fc91de823bc1c604db6e3ac716e0
Ramya.U <ramyayadav2016@gmail.com>
2020-11-17 21:54:31 +0530

    [Ramya] Add . Added POCO and DAO files

---
+++ IndianStateCensusAnalyser/DTO/CensusDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyser.DTO
+{
+    public class CensusDTO
+    {
+        public int srNo;
+        public string stateName;
+        public int tin;
+        public string stateCode;
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CensusDTO"/> class.
+        /// </summary>
+        /// <param name="stateCodeDAO">The state code DAO.</param>
+        public CensusDTO(StateCodeDAO stateCodeDAO)
+        {
+            this.srNo = stateCodeDAO.srNo;
+            this.stateName = stateCodeDAO.stateName;
+            this.tin = stateCodeDAO.tin;
+            this.stateCode = stateCodeDAO.stateCode;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CensusDTO"/> class.
+        /// </summary>
+        /// <param name="censusDataDAO">The census data DAO.</param>
+        public CensusDTO(CensusDataDAO censusDataDAO)
+        {
+            this.state = censusDataDAO.state;
+            this.population = censusDataDAO.population;
+            this.area = censusDataDAO.area;
+            this.density = censusDataDAO.density;
+        }
+    }
+}
