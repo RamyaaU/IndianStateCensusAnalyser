36eb536fc10dc2d6f80e8e2ba979f0bd64a3c62e
Ramya.U <ramyayadav2016@gmail.com>
2020-11-17 22:02:13 +0530

    [Ramya] Add . Added method to check exceptions

---
+++ IndianStateCensusAnalyser/CheckFileExceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CheckFileExceptions
+    {
+        /// <summary>
+        /// Checks the exceptions.
+        /// </summary>
+        /// <param name="csvPath">The CSV path.</param>
+        /// <exception cref="IndianStateCensusAnalyser.CensusAnalyserException">
+        /// File Does Not Exist
+        /// or
+        /// File Path is Improper
+        /// </exception>
+        public static void CheckExceptions(string csvPath)
+        {
+            if (File.Exists(csvPath) == false)
+            {
+                throw new CensusAnalyserException("File Does Not Exist", CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
+            }
+            if (Path.GetExtension(csvPath) != ".csv")
+            {
+                throw new CensusAnalyserException("File Path is Improper", CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
+            }
+
+        }
+    }
+}
