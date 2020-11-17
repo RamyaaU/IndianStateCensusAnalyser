6dddaad239fcef5b58071a9f372240c087fd2b81
Ramya.U <ramyayadav2016@gmail.com>
2020-11-17 21:56:37 +0530

    [Ramya] Add .Added exception class

---
+++ IndianStateCensusAnalyser/CensusAnalyserException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CensusAnalyserException : Exception
+    {
+        /// <summary>
+        /// enum variables declaration
+        /// </summary>
+        public enum ExceptionType
+        {
+            FILE_NOT_FOUND,
+            INVALID_FILE_TYPE,
+            INCORRECT_DELIMITER,
+            INCORRECT_HEADER,
+            NO_SUCH_COUNTRY
+        }
+
+        /// <summary>
+        /// The exception type
+        /// </summary>
+        public ExceptionType eType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CensusAnalyserException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exceptionType">Type of the exception.</param>
+        public CensusAnalyserException(string message, ExceptionType exceptionType) : base(message)
+        {
+            this.eType = exceptionType;
+        }
+    }
+}
