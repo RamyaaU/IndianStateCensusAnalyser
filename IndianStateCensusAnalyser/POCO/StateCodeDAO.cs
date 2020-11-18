847181481f80276f3938885cf7a5b55b48ea0124
Ramya.U <ramyayadav2016@gmail.com>
2020-11-18 12:06:31 +0530

    [Ramya] Refactor . Modified StateCodeDAO.cs and CensusDataDAO.cs

--- IndianStateCensusAnalyser/POCO/StateCodeDAO.cs
+++ IndianStateCensusAnalyser/POCO/StateCodeDAO.cs
@@ -6,7 +6,7 @@ namespace IndianStateCensusAnalyser.POCO
 {
     public class StateCodeDAO
     {
-        public int srNo;
+        public int serialNumber;
         public string stateName;
         public int tin;
         public string stateCode;
@@ -14,17 +14,17 @@ namespace IndianStateCensusAnalyser.POCO
         /// <summary>
         /// Initializes a new instance of the <see cref="StateCodeDAO"/> class.
         /// </summary>
-        /// <param name="srNo">The sr no.</param>
-        /// <param name="stateName">Name of the state.</param>
-        /// <param name="tin">The tin.</param>
-        /// <param name="stateCode">The state code.</param>
-        public StateCodeDAO(string srNo, string stateName, string tin, string stateCode)
+        /// <param name="v1">The v1.</param>
+        /// <param name="v2">The v2.</param>
+        /// <param name="v3">The v3.</param>
+        /// <param name="v4">The v4.</param>
+        public StateCodeDAO(string v1, string v2, string v3, string v4)
         {
-            this.srNo = Convert.ToInt32(srNo);
-            this.stateName = stateName;
-            this.tin = Convert.ToInt32(tin);
-            this.stateCode = stateCode;
+            this.serialNumber = Convert.ToInt32(v1);
+            this.stateName = v2;
+            this.tin = Convert.ToInt32(v3);
+            this.stateCode = v4;
         }
-
     }
 }
+
